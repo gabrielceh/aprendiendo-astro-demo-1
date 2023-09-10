0f781663028fb0f1a9ce2a1561a30bb185ea56ac
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-09-10 12:07:06 -0500

    páginas estaticas, dinamicas y ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	integrations: [tailwind()],
+	// output:"server" // -> si queremos que toda la pag sea server side
+	output: 'hybrid', // -> si queremos que combine entre ssr, static y dynamic
 });
